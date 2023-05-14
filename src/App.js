cd87321a971d4d9789a4b26e015a336c619d0950
ThienNguyen0609 <nguyenhoanthien7j2001@gmail.com>
2023-05-14 23:47:54 +0700

    init practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function Mybtns() {
+  const [count, setCount] = useState(0);
+  function Inc() {
+    setCount(count+1);
+  }
+  function Dec() {
+    setCount(count-1);
+  }
+  return (
+    <div class="cnt">
+      <h3>COUNTER</h3>
+      <h3>{count}</h3>
+      <div class="btns">
+        <button class="btn" onClick={Inc}>
+          Increase
+        </button>
+        <button class="btn" onClick={Dec}>
+          Descrease
+        </button>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div class="container">
+      <Mybtns />
     </div>
   );
 }
